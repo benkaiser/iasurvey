c5ff41f1b9e5d96f12aa033348337940bb89ecb8
Benjamin Kaiser <benjaminjkaiser@gmail.com>
2014-09-18 18:29:16 +1000

    add: split survey into sections based on section breaks

--- static/js/survey.js
+++ static/js/survey.js
@@ -6,10 +6,13 @@ IAApp.addRegions({
   surveyRegion: "#surveyContainer",
 });
 
-IAApp.addInitializer(function(options){
+var sections;
+IAApp.addInitializer(function(options) {
+  // load the data
+   sections = splitBySections(survey.form.fields);
   // load the survey view
   this.cv = new SurveyView({});
-  IAApp.contentRegion.show(this.cv);
+  IAApp.surveyRegion.show(this.cv);
 });
 
 var SurveyView = Backbone.View.extend({
@@ -17,12 +20,32 @@ var SurveyView = Backbone.View.extend({
   events: {
     "click #next": "nextSection"
   },
-  initialize: function(){
+  initialize: function() {
     // initialise survey
   },
-  render: function(){
+  render: function() {
     // render survey
   }
 });
+
+// this function is used to split the survey onto multiple pages
+function splitBySections(fields){
+  var sections = [];
+  var index = 0;
+  for(var x = 0; x < fields.length; x++){
+    // do we want a new page and is the current page not empty?
+    if(fields[x].field_type == "section_break"){
+      if(sections.length != index){
+        index++;
+      }
+    } else {
+      if(sections.length == index){
+        sections.push([]);
+      }
+      sections[index].push(fields[x]);
+    }
+  }
+  return sections;
+}
 
 IAApp.start();
